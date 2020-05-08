0caae1c6a433b7c0a1190ce2cf84cabc7cb8c8cd
Damien Braillard <contact@damien-braillard.ch>
2020-05-08 20:28:14 +0200

    Add basic translation extension

---
+++ src/LogMeTender/Extensions/Translate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+
+namespace LogMeTender.Extensions
+{
+    /// <summary>
+    /// A markup extension that provides translations
+    /// </summary>
+    public class Translate : MarkupExtension
+    {
+        private readonly string _originalText;
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="Translate"/> class.
+        /// </summary>
+        /// <param name="originalText">The original text that must be translated.</param>
+        public Translate(string originalText)
+        {
+            _originalText = originalText;
+        }
+
+        /// <inheritdoc/>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+#warning TODO: This will have to be implemented once.
+            return _originalText;
+        }
+    }
+}
